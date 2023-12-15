f3484f84a83ae37159b67b12d48d72069a9be7cc
Zack Dowd <zdowd2796@gmail.com>
2023-12-15 09:40:28 -0700

    added all endpoints

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import  datetime
 
 USERNAME = "cptptag"
 TOKEN = "jfpo153j67li457li4"
@@ -11,7 +12,7 @@ user_parameters = {
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
-
+#POST new profile
 # response = requests.post(url=pixela_endpoint, json=user_parameters)
 # print(response.text)
 
@@ -29,6 +30,32 @@ graph_parameters = {
 headers = {
     "X-USER-TOKEN": TOKEN
 }
+#POST graph
+# response = requests.post(url=graph_endpoint, json=graph_parameters, headers=headers)
+# print(response.text)
+today = datetime.now()
+today_formatted = today.strftime("%Y%m%d")
 
-response = requests.post(url=graph_endpoint, json=graph_parameters, headers=headers)
+pixel_post = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_parameters["id"]}"
+pixel_update = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_parameters["id"]}/{today_formatted}"
+pixel_delete = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_parameters["id"]}/{today_formatted}"
+
+pixel_parameters = {
+    "date": today_formatted,
+    "quantity": "20"
+}
+# #POST pixel
+response = requests.post(url=pixel_post, json=pixel_parameters, headers=headers)
 print(response.text)
+
+pixel_put_params = {
+    "quantity": "15"
+}
+
+# #PUT Pixel
+# response = requests.put(url=pixel_update, json=pixel_put_params, headers=headers)
+# print(response.text)
+
+# #DELETE Pixel
+# response = requests.delete(url=pixel_delete, headers=headers)
+# print(response.text)
